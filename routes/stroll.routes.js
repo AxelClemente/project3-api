f9f4e992db41cef4f85247949aee1f17654fd80e
Axel <axel_clemente@yahoo.com>
2023-02-28 20:18:54 +0100

    stroll and user updated

--- routes/stroll.routes.js
+++ routes/stroll.routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
 const User = require("../models/User.model");
 const Rating = require("../models/Rating.model");
 const Stroll = require("../models/Stroll.model");
@@ -8,83 +7,56 @@ const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 
 // GET ALL the Strolls
 router.get("/", (req, res, next) => {
-  Stroll.find()
-    .then((strolls) => {
-      console.log(res.data);
-      res.json(strolls);
+    Stroll.find()
+    .then(strolls =>{
+        console.log(res.data);
+        res.json(strolls);
     })
-    .catch((err) => console.log(err));
-});
-
-//*Testing*
-// router.get("/", async (req, res, next) => {
-//   try {
-//     const strolls = await Stroll.find({}, 'city');
-//     const cities = strolls.map(stroll => stroll.city);
-//     res.json(cities);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
-
-//Create Stroll
-router.post("/", async (req, res, next) => {
-  try {
+    .catch(err => console.log(err))
+})
 // Create Strolls
 // router.post("/", async (req, res, next) => {
 //   try {
-
 //     const newStroll = req.body;
 //     // console.log("This is the new stroll:",newStroll)
     
 //     const stroll = await Stroll.create(newStroll);
 //     console.log("This is the new stroll after using create:",stroll)
-
 //     const userId = req.body.userId;
 //     console.log("testing userId:",userId)
-
 
 //    // Now add the Id value of the new object to other model: User - property: stroll
 //     const user = await User.findById(userId);
 //     console.log("testing user:",user)
 //     user.stroll.push(stroll._id);
 //     await user.save();
-
 //   // Response to client!
 //     res.json(stroll);
 //     console.log("Quiero saber esta info ",stroll)
-
 //   } catch (error) {
 //     console.log(error);
 //   }
 // });
-
 
 // Edit a Stroll
 // router.put("/:id/edit", isAuthenticated, async (req, res) => {
 //   try {
 //     const { id } = req.params;
 //     const { title, description, city, duration, distance, budget, guide, country,description1, description2, description3, description4, description5, description6, stops1, stops2, stops3, stops4, stops5, stops6 } = req.body;
-
 //     console.log("Updating stroll with ID:", id);
 //     console.log("New data:", req.body);
-
 //     // Find the stroll by ID
 //     const stroll = await Stroll.findById(id);
-
 //     // If stroll not found, return error response
 //     if (!stroll) {
 //       console.log("Stroll not found with ID:", id);
 //       return res.status(404).json({ message: "Stroll not found" });
 //     }
-
 //     // Check if user is the owner of the stroll
 //     if (req.user.id !== stroll.user.toString()) {
 //       console.log("User not authorized to edit stroll with ID:", id);
 //       return res.status(403).json({ message: "You are not authorized to edit this stroll" });
 //     }
-
 //     // Update the stroll with the new data
 //     stroll.title = title || stroll.title;
 //     stroll.country = country || stroll.country;
@@ -106,32 +78,25 @@ router.post("/", async (req, res, next) => {
 //     stroll.distance = distance || stroll.distance;
 //     stroll.budget = budget || stroll.budget;
 //     stroll.guide = guide || stroll.guide;
-
 //     console.log("Updated stroll data:", stroll);
-
 //     // Save the updated stroll to the database
 //     const updatedStroll = await stroll.save();
-
 //     console.log("Stroll with ID", id, "updated successfully");
-
 //     res.json(updatedStroll);
 //   } catch (error) {
 //     console.error(error);
 //     res.status(500).json({ message: "Internal server error" });
 //   }
 // });
-
 router.put("/:id", async (req, res) => {
   const { id } = req.params;
   const updatedData = req.body;
   console.log("probando si sirve esto o no....",updatedData)
-
   try {
     const updatedStroll = await Stroll.findByIdAndUpdate(id, updatedData, { new: true });
     if (!updatedStroll) {
       return res.status(404).json({ message: "Stroll not found" });
     }
-
     res.json(updatedStroll);
   } catch (error) {
     console.error(error);
@@ -140,54 +105,27 @@ router.put("/:id", async (req, res) => {
 });
 
 
-
 //Test to add both propertys at the same time
 router.post("/", async (req, res, next) => {
   try {
     const newStroll = req.body;
     const userId = newStroll.userId;
-
     const user = await User.findById(userId);
     if (!user) {
       return res.status(404).send("User not found");
     }
-
     const stroll = await Stroll.create(newStroll);
     user.stroll.push(stroll._id);
     await user.save();
-
     // Update the `user` property in the `Stroll` model
     stroll.user = user._id;
     await stroll.save();
-
     res.json(stroll);
   } catch (error) {
     console.log(error);
     res.status(500).send("Internal Server Error");
   }
 });
-    const userId = newStroll.userId;
-
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).send("User not found");
-    }
-
-    const stroll = await Stroll.create(newStroll);
-    user.stroll.push(stroll._id);
-    await user.save();
-
-    // Update the `user` property in the `Stroll` model
-    stroll.user = user._id;
-    await stroll.save();
-
-    res.json(stroll);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
 
 
 router.get("/user/:userId", async (req, res, next) => {
@@ -200,29 +138,6 @@ router.get("/user/:userId", async (req, res, next) => {
     res.status(500).send("Internal Server Error");
   }
 });
-router.get("/user/:userId", async (req, res, next) => {
-  try {
-    const { userId } = req.params;
-    const strolls = await Stroll.find({ user: userId });
-    res.json({ strolls });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
-// Updated Strolls
-// router.put("/:id", async (req, res, next) => {
-//     try {
-//       const updatedStroll = req.body;
-//       const updatedDoc = await Stroll.findByIdAndUpdate(req.params.id, updatedStroll, { new: true });
-//       res.json(updatedDoc);
-//     } catch (err) {
-//       console.error(err);
-//       res.status(500).send("Internal Server Error");
-//     }
-//   });
-
 
 
 // Updated Strolls
@@ -236,27 +151,18 @@ router.get("/user/:userId", async (req, res, next) => {
 //       res.status(500).send("Internal Server Error");
 //     }
 //   });
-
 // Stroll Detail
-router.get("/:id", (req, res) => {
-  Stroll.findById(req.params.id)
-    .then((strolls) => res.json(strolls))
-    .catch((err) => console.error(err));
-});
 router.get("/:id", (req, res)=> {
     Stroll.findById(req.params.id).populate("user")
         .then(strolls => res.json(strolls))
         .catch(err => console.error(err))
 })
-
 // router.get("/:id",(req,res) => {
-
 //   try{
 //    const stroll =  Stroll.findById(req.params.id).populate("user");
 //     if (!stroll) {
 //       return res.status(404).json({message: "Stroll not found"})
 //     }
-
 //     res.json(stroll)
 //   } catch(error) {
 //     console.error(error);
@@ -265,62 +171,36 @@ router.get("/:id", (req, res)=> {
   
 // })
 
-
 // Stroll removed
-// router.delete("/:id", async (req, res, next) => {
-//     try {
-//       const deletedDoc = await Stroll.findByIdAndDelete(req.params.id);
-//       if (!deletedDoc) {
-//         return res.status(404).send("Document not found");
-//       }
-//       res.json(deletedDoc);
-//     } catch (err) {
-//       console.error(err);
-//       res.status(500).send("Internal Server Error");
-//     }
-// });
-
-// Testing
 router.delete("/:id", async (req, res, next) => {
-  try {
-    const deletedDoc = await Stroll.findByIdAndDelete(req.params.id);
-    if (!deletedDoc) {
-      return res.status(404).send("Document not found");
+    try {
+      const deletedDoc = await Stroll.findByIdAndDelete(req.params.id);
+      if (!deletedDoc) {
+        return res.status(404).send("Document not found");
+      }
+      res.json(deletedDoc);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
     }
+  });
 
-    // Find the corresponding user and remove the deleted stroll ID from their `stroll` property
-    const user = await User.findOne({ stroll: deletedDoc._id });
-    if (!user) {
-      return res.status(404).send("User not found");
-    }
-    user.stroll = user.stroll.filter((id) => id !== deletedDoc._id);
-    await user.save();
-
-    res.json(deletedDoc);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal Server Error");
-  }
-});
 
 // Add a rating to a stroll
 router.put("/:id", async (req, res) => {
   const { rating } = req.body;
   const { id } = req.params;
-
   try {
     const stroll = await Stroll.findById(id);
     if (!stroll) {
       return res.status(404).json({ message: "Stroll not found" });
     }
-
     const existingRating = await Rating.findOne({ stroll: id });
     if (existingRating) {
       existingRating.rating = rating;
       await existingRating.save();
       return res.status(200).json(existingRating);
     }
-
     const newRating = new Rating({
       rating,
       stroll: id,
@@ -332,8 +212,4 @@ router.put("/:id", async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
-
 module.exports = router;
-
-
-
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,46 +1,37 @@
 const express = require("express");
 const router = express.Router();
-
 const Rating = require("../models/Rating.model");
 const Stroll = require("../models/Stroll.model");
 const User = require("../models/User.model");
-
 const { isAuthenticated } = require("../middleware/jwt.middleware.js");
-
 // Update User
 router.put("/users", isAuthenticated, (req, res, next) => {
   const { profilePicture } = req.body;
   const userId = req.payload._id;
-
   User.findByIdAndUpdate(userId, { profilePicture }, { new: true })
     .then(({ __id, email, username, country, city, profilePicture }) =>
       res.json({ __id, email, username, country, city, profilePicture })
     )
     .catch((err) => console.error(err));
 });
-
 // Get all the users
 router.get("/users", isAuthenticated, (req, res, next) => {
   // If JWT token is valid the payload gets decoded by the
   // isAuthenticated middleware and is made available on `req.payload`
   console.log(`req.payload`, req.payload);
-
   // Send back the token payload object containing the user data
   res.status(200).json(req.payload);
 });
-
 // Push to list an Stroll Id (FAVORITES)
 router.post("/:userId", async (req, res) => {
   const { userId } = req.params;
   console.log("quiero saber userId:", userId);
   const { strollId } = req.body;
-
   try {
     const user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-
     let objectId;
     try {
       objectId = strollId;
@@ -48,14 +39,11 @@ router.post("/:userId", async (req, res) => {
       console.error(error);
       return res.status(400).json({ message: "Invalid strollId" });
     }
-
     if (user.list.includes(objectId)) {
       return res.status(400).json({ message: "Stroll already in list" });
     }
-
     user.list.push(objectId); // <----------------------------------------------------- Goal!
     await user.save();
-
     const populatedUser = await User.findById(userId).populate("list");
     res.status(200).json(populatedUser);
     
@@ -64,25 +52,20 @@ router.post("/:userId", async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 });
-
 //remove a Stroll from the favorites
 router.delete("/users/:userId/:strollId", async (req, res) => {
   const { userId, strollId } = req.params;
-
   try {
     const user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-
     const index = user.list.indexOf(strollId);
     if (index < 0) {
       return res.status(404).json({ message: "Stroll not found in list" });
     }
-
     user.list.splice(index, 1);
     await user.save();
-
     const populatedUser = await User.findById(userId).populate("list");
     res.status(200).json(populatedUser);
   } catch (error) {
@@ -90,75 +73,45 @@ router.delete("/users/:userId/:strollId", async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 });
-
-
-
-// Get strolls associated with a user
-router.get("/strolls/user/:userId", async (req, res) => {
-  const { userId } = req.params;
+// Get user by ID including their list of favorite strolls
+router.get("/users/:userId", isAuthenticated, async (req, res) => {
+  const userId = req.params.userId;
   try {
-    const strolls = await Stroll.find({ user: userId }).populate("user");
-    res.json({ strolls });
+    const user = await User.findById(userId).populate("list");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
   }
 });
-
+//Pus the strollId into the stroll property of the user model
+router.post("/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const { strollId } = req.body;
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    if (user.stroll.includes(strollId)) {
+      return res.status(400).json({ message: "Stroll already in list" });
+    }
+    user.stroll.push(strollId);
+    await user.save();
+    const populatedUser = await User.findById(userId).populate("stroll");
+    res.status(200).json(populatedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 //test axel
 
 
 
-
-//Pus the strollId into the stroll property of the user model
-router.post("/:userId", async (req, res) => {
-  const { userId } = req.params;
-  const { strollId } = req.body;
-
-  try {
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    if (user.stroll.includes(strollId)) {
-      return res.status(400).json({ message: "Stroll already in list" });
-    }
-
-    user.stroll.push(strollId);
-    await user.save();
-    const populatedUser = await User.findById(userId).populate("stroll");
-    res.status(200).json(populatedUser);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error" });
-  }
-});
-
-//Pus the strollId into the stroll property of the user model
-router.post("/:userId", async (req, res) => {
-  const { userId } = req.params;
-  const { strollId } = req.body;
-
-  try {
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    if (user.stroll.includes(strollId)) {
-      return res.status(400).json({ message: "Stroll already in list" });
-    }
-
-    user.stroll.push(strollId);
-    await user.save();
-    const populatedUser = await User.findById(userId).populate("stroll");
-    res.status(200).json(populatedUser);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error" });
-  }
-});
 
 module.exports = router;
